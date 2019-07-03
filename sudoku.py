cb36ee453c778bf8c2d420d842142463bf2dc519
Donáth Botond <belgianmaniax@gmail.com>
2019-07-03 11:54:10 +0200

    Added cheatsheet

--- sudoku.py
+++ sudoku.py
@@ -20,7 +20,7 @@ def print_grid(rows_file):
     for i in range(3):
         line_border += "-" * 11 + make_yellow(":")
 
-    print(box_border, " " * 12, )
+    print(box_border + " " * 12 + box_border)
     for j, num_row in enumerate(rows_file):
         row_list = []
         for num in num_row:
@@ -35,26 +35,16 @@ def print_grid(rows_file):
                 row += f" {cell} " + make_yellow("|")
             else:
                 row += f" {cell} |"
+        if j % 3 == 1:
+            row += " " * 12 + make_yellow("|")
+            for k in range(j, j + 3):
+                row += str(k).center(11) + make_yellow("|")
+        else:
+            row += " " * 12 + make_yellow("|")
+            for k in range(3):
+                row += " " * 11 + make_yellow("|")
         print(row)
         if j % 3 == 2:
-            print(box_border)
+            print(box_border + " " * 12 + box_border)
         else:
             print(line_border)
-
-
-clear()
-
-index_dict = {}
-
-for x in range(81):
-    row = x // 9
-    column = x % 9
-    box = 3 * (row // 3) + column // 3
-    index_dict[x] = [row, column, box]
-
-with open("test_grid.txt", "r") as grid_file:
-    rows_file = []
-    for line in grid_file.read().splitlines():
-        rows_file.append(line)
-
-print_grid(rows_file)
